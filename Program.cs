417cc311c768a513b947b1ab3365f4c55514d695
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2024-12-09 09:00:14 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NPCWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erstelle eine Klasse NPC mit den Feldern Dialog Lebenspunkte und Name.
+            //Erstelle eine Methode um mit dem NPC zu interagieren bzw um ausgaben aus dem Feld Dialog zu generieren
+            //Es sollen auf Basis dieser Klasse zwei Kind-Klassen erzeugt werden und zwar NPCFeind und NPCFreund
+            //Bei einem freundlichen NPC soll es eine möglichkeit geben zu handeln (Trading muss nicht implementiert werden) lediglich Ausgabe
+            //Bei einem feindlichen NPC soll es zu einem Kampf kommen (Daselbe wie beim Trading)
+            //Erstellt aus den Kindklassen 4 Objekte also 2 freundliche NPCs und zwei feindliche.
+            //Erstelle in der Program.cs eine Methode um auf die verschiedenen NPCs zuzugreifen bzw. mit diesen zu Interagieren.
+
+            //Für Fortgeschrittene implemtiert Handeln und Kämpfen passt dafür die Felder ggf. an 
+
+            //Tipp: Für das Inventory recherchieren zum Thema Listen bzw dynamische Listen
+
+
+
+
+
+        }
+    }
+}
