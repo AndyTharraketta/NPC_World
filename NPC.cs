bea16efed584bb690f68ca8ae06ededc0e698b66
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2024-12-09 12:16:55 +0100

    korrektur

--- NPC.cs
+++ NPC.cs
@@ -8,98 +8,145 @@ namespace NPCWorld
 {
     internal class NPC
     {
-        string dialog;
-        int lebenspunkte;
-        string name;
+        //string dialog;
+        //int lebenspunkte;
+        //string name;
 
-        public NPC(string dialog, int lebenspunkte, string name)
+        //public NPC(string dialog, int lebenspunkte, string name)
+        //{
+        //    this.dialog = dialog;
+        //    this.lebenspunkte = lebenspunkte;
+        //    this.name = name;
+        //}
+
+        //public string Dialog { get => dialog; set => dialog = value; }
+        //public int Lebenspunkte { get => lebenspunkte; set => lebenspunkte = value; }
+        //public string Name { get => name; set => name = value; }
+
+        //public void VendorDialog(string userName)
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine($"\tHallo {userName}!\nMein Name lautet {name}!\nIch habe verschiedene Waren im Angebot!");
+        //    Console.ForegroundColor = ConsoleColor.DarkYellow;
+        //    Console.WriteLine("\nTaste drücken zum fortsetzten");
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.ReadKey();
+
+        //}
+
+        //public void EnemyDialog(string userName)
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine($"Argh! Ich bin {name}! Ich will mit Dir kämpfen!");
+        //    Console.ForegroundColor = ConsoleColor.DarkYellow;
+        //    Console.WriteLine("\nTaste drücken zum fortsetzten");
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.ReadKey();
+
+        //}
+
+        //public void VendorStats()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine($"\t\t-Stats-\n\n\tName:\t\t{name}\n\tLP:\t\t{lebenspunkte} Punkte");
+        //    Console.ForegroundColor = ConsoleColor.DarkYellow;
+        //    Console.WriteLine("\nTaste drücken zum fortsetzten");
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.ReadKey();
+        //}
+
+        //public void EnemyStats()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine($"\t\t-Stats-\n\n\tName:\t\t{name}\n\tLP:\t\t{lebenspunkte} Punkte");
+        //    Console.ForegroundColor = ConsoleColor.DarkYellow;
+        //    Console.WriteLine("\nTaste drücken zum fortsetzten");
+        //    Console.ForegroundColor = ConsoleColor.Yellow;
+        //    Console.ReadKey();
+        //}
+
+        //public void VendorMenu(string userName)
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine("Wähle eine Aktion:\n\t\t-1-Gespräch\n\t\t-2-Inspitieren");
+        //    string input = Console.ReadLine();
+        //    if (input == "1") VendorDialog(userName);
+        //    else if (input == "2") VendorStats();
+        //    else
+        //    {
+        //        Console.ForegroundColor = ConsoleColor.Red;
+        //        Console.WriteLine("Falsche Eingabe!");
+        //        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        //        Console.WriteLine("\nTaste drücken zum fortsetzten");
+        //        Console.ForegroundColor = ConsoleColor.Yellow;
+        //        Console.ReadKey();
+        //    }
+        //}
+
+        //public void EnemyMenu()
+        //{
+        //    Console.Clear();
+        //    Console.WriteLine("Wähle eine Aktion:\n\t\t-1-Gespräch\n\t\t-2-Inspitieren");
+        //    string input = Console.ReadLine();
+        //    if (input == "1") EnemyDialog(",");
+        //    else if (input == "2") EnemyStats();
+        //    else
+        //    {
+        //        Console.ForegroundColor = ConsoleColor.Red;
+        //        Console.WriteLine("Falsche Eingabe!");
+        //        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        //        Console.WriteLine("\nTaste drücken zum fortsetzten");
+        //        Console.ForegroundColor = ConsoleColor.Yellow;
+        //        Console.ReadKey();
+        //    }
+
+        // Korrektur
+
+        protected int lp = 100;
+        protected string name = "Unbekannter";
+        protected string[] dialogue = { "..." };
+
+        public int Lp { get => lp; set => lp = value; }
+        public string Name { get => name; }
+        public string[] Dialogue { get => dialogue; }
+
+        public NPC()
         {
-            this.dialog = dialog;
-            this.lebenspunkte = lebenspunkte;
+
+        }
+
+        public NPC(int lp, string name, string[] dialogue)
+        {
+            this.lp = lp;
             this.name = name;
+            this.dialogue = dialogue;
         }
 
-        public string Dialog { get => dialog; set => dialog = value; }
-        public int Lebenspunkte { get => lebenspunkte; set => lebenspunkte = value; }
-        public string Name { get => name; set => name = value; }
-
-        public void VendorDialog(string userName)
+        public void Interact(NPC npc)
         {
-            Console.Clear();
-            Console.WriteLine($"\tHallo {userName}!\nMein Name lautet {name}!\nIch habe verschiedene Waren im Angebot!");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("\nTaste drücken zum fortsetzten");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ReadKey();
-
-        }
-
-        public void EnemyDialog(string userName)
-        {
-            Console.Clear();
-            Console.WriteLine($"Argh! Ich bin {name}! Ich will mit Dir kämpfen!");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("\nTaste drücken zum fortsetzten");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ReadKey();
-
-        }
-
-        public void VendorStats()
-        {
-            Console.Clear();
-            Console.WriteLine($"\t\t-Stats-\n\n\tName:\t\t{name}\n\tLP:\t\t{lebenspunkte} Punkte");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("\nTaste drücken zum fortsetzten");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ReadKey();
-        }
-
-        public void EnemyStats()
-        {
-            Console.Clear();
-            Console.WriteLine($"\t\t-Stats-\n\n\tName:\t\t{name}\n\tLP:\t\t{lebenspunkte} Punkte");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("\nTaste drücken zum fortsetzten");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ReadKey();
-        }
-
-        public void VendorMenu(string userName)
-        {
-            Console.Clear();
-            Console.WriteLine("Wähle eine Aktion:\n\t\t-1-Gespräch\n\t\t-2-Inspitieren");
-            string input = Console.ReadLine();
-            if (input == "1") VendorDialog(userName);
-            else if (input == "2") VendorStats();
-            else
+            foreach(string text in dialogue)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Falsche Eingabe!");
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("\nTaste drücken zum fortsetzten");
-                Console.ForegroundColor= ConsoleColor.Yellow;
+                string output = "";
+                foreach(char i in text)
+                {
+                    output += i;
+                    Console.Clear();
+                    Console.WriteLine(output);
+                    Thread.Sleep(30);
+                }
                 Console.ReadKey();
+                Console.Clear();
             }
-        }
-
-        public void EnemyMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Wähle eine Aktion:\n\t\t-1-Gespräch\n\t\t-2-Inspitieren");
-            string input = Console.ReadLine();
-            if (input == "1") EnemyDialog(",");
-            else if (input == "2") EnemyStats();
-            else
+            if(npc is NPCFriend friend)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Falsche Eingabe!");
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("\nTaste drücken zum fortsetzten");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.ReadKey();
+                friend.Trade();
+            }
+            else if(npc is NPCEnemy enemy)
+            {
+                enemy.Fight();
             }
         }
 
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -19,34 +19,69 @@ namespace NPCWorld
             //Tipp: Für das Inventory recherchieren zum Thema Listen bzw dynamische Listen
 
             Console.ForegroundColor = ConsoleColor.Yellow;
+
+            //NPC enemy = new NPC(",", 300, "Kevin");
+            //NPC vendor = new NPC(",",150,"Axel");
+
+            //Console.WriteLine($"Hallo User! Gib bitte Deinen Namen ein: ");
+            //string userName = Console.ReadLine();
+
+            ////vendor.VendorDialog(userName);     // Test
+
+            //while (true)
+            //{ 
+            //    Console.Clear();
+            //    Console.WriteLine($"Wähle eine Aktion:\n\t-1-{enemy.Name}\n\t-2-{vendor.Name}");
+            //    string input = Console.ReadLine();
+            //    if (input == "1") enemy.EnemyMenu();
+            //    else if (input == "2") vendor.VendorMenu(userName);
+            //    else
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.WriteLine("Falsche Eingabe!");
+            //        Console.ForegroundColor = ConsoleColor.DarkYellow;
+            //        Console.WriteLine("\nTaste drücken zum fortsetzten");
+            //        Console.ForegroundColor = ConsoleColor.Yellow;
+            //        Console.ReadKey();
+            //    }
+
+            // Korrektur
+
+            //NPC testNPC = new NPC(60, "Karsten",["Hallo ich bin Karsten!","Freut mich Dich kennenzulernen!","Wie geht es Dir?"]);
+
+            //testNPC.Interact();
+
+            Menu();
             
-            NPC enemy = new NPC(",", 300, "Kevin");
-            NPC vendor = new NPC(",",150,"Axel");
 
-            Console.WriteLine($"Hallo User! Gib bitte Deinen Namen ein: ");
-            string userName = Console.ReadLine();
+            Console.ResetColor();
+        }
 
-            //vendor.VendorDialog(userName);     // Test
+        public static void Menu()
+        {
+            NPCFriend npc1 = new NPCFriend(50, "Maria", ["Hallo ich bin Maria aus Flußwald.","Warst Du schon mal in Flußwald?","Ich habe einige Waren von dort die ich zu Handeln anbiete."]);
+            NPCFriend npc2 = new NPCFriend(200, "Alfred", ["Ich verkaufe Dir die besten Waren","Ich weiß nicht wem sie vorher gehört haben.","Was sagst Du dazu?"]);
+            NPCEnemy npc3 = new NPCEnemy(300,"Böser Bör", ["Grrrrr"]);
+            NPCEnemy npc4 = new NPCEnemy(120,"Fitz den Argonier", ["Dein Gold ...","UND DEIN LEBEN"]);
 
             while (true)
-            { 
+            {
                 Console.Clear();
-                Console.WriteLine($"Wähle eine Aktion:\n\t-1-{enemy.Name}\n\t-2-{vendor.Name}");
+                Console.WriteLine($"Mit welchem NPC möchtest Du interagieren? (1 - 4)\n\n1. {npc1.Name}\n\n2. {npc2.Name}\n\n3. {npc3.Name}\n\n4. {npc4.Name}\n");
                 string input = Console.ReadLine();
-                if (input == "1") enemy.EnemyMenu();
-                else if (input == "2") vendor.VendorMenu(userName);
+
+                if (input == "1") npc1.Interact(npc1);
+                else if (input == "2") npc2.Interact(npc2);
+                else if (input == "3") npc3.Interact(npc3);
+                else if (input == "4") npc4.Interact(npc4);
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Falsche Eingabe!");
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.WriteLine("\nTaste drücken zum fortsetzten");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.ReadKey();
+                    Console.WriteLine("Eingabe ist ungültig!");
                 }
-               
             }
 
         }
+
     }
 }
+
